76b9687cc7cb39b169e35d8f8cd357e35bdd959d
hegdeashwin <ashwin.hegde3@gmail.com>
2016-11-17 01:22:15 +0530

    Adds Karma config

---
+++ test.webpack.config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// angular 1.5.x config => fix for angular2: todo
+// import 'angular';
+// import 'angular-mocks/angular-mocks';
+//
+// var testsContext = require.context("./src/app/", true, /-test$/);
+// testsContext.keys().forEach(testsContext);
