d8795495a3875512b232fc74a3917b29b09869e9
hegdeashwin <ashwin.hegde3@gmail.com>
2016-11-16 23:15:39 +0530

    Fixes TS errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { HomeComponent } from './pages/home/home';
+import { NgModule } from "@angular/core";
+import { BrowserModule }  from "@angular/platform-browser";
+import { HomeComponent } from "./pages/home/home";
 
 @NgModule({
   imports: [BrowserModule],
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: require('./home-tpl.html')
+  selector: "my-app",
+  templateUrl: require("./home-tpl.html")
 })
 
 export class HomeComponent { }
