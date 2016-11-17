529c27211081ec7c240fc536eec77e3ebc2f31e9
hegdeashwin <ashwin.hegde3@gmail.com>
2016-11-18 01:27:06 +0530

    Adds Strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { rootRouterConfig } from "./router/app.routes";
 import { LayoutComponent } from "./pages/layout/layout.component";
@@ -10,7 +11,8 @@ import { AboutComponent } from "./pages/about/about.component";
 @NgModule({
   declarations: [LayoutComponent, HomeComponent, AboutComponent],
   imports: [BrowserModule, RouterModule.forRoot(rootRouterConfig)],
-  bootstrap: [LayoutComponent]
+  bootstrap: [LayoutComponent],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 
 export class AppModule { }
